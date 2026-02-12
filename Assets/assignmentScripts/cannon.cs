7ddaf40fac89ba4c6452b23fa91ec9f6ec812239
Mingyu Chi <chimi@shernet.sheridancollege.ca>
2026-02-12 17:52:27 -0500

    made cannon

---
+++ Assets/assignmentScripts/cannon.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class cannon : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mouse.current.leftButton.isPressed == true)
+        {
+            Vector2 mousepos = Camera.main.ScreenToWorldPoint(UnityEngine.InputSystem.Mouse.current.position.ReadValue());
+            Vector2 D = mousepos - (Vector2)transform.position;
+            transform.up = D;
+        }
+    }
+
+    public void moveRightLeft(float n)
+    {
+        Vector3 newpos = transform.position;
+        newpos.x = n * 30;
+        transform.position = newpos;
+    }
+}
